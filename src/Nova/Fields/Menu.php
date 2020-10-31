76ebfca5a0d8f525f168dd30a753fd861bf621af
Ismail Zare <zarehesmaiel@gmail.com>
2020-10-31 13:27:17 +0330

    Added menu missing fields

--- src/Nova/Fields/Menu.php
+++ src/Nova/Fields/Menu.php
@@ -3,9 +3,11 @@
 namespace Armincms\Sofre\Nova\Fields;
 
 use Illuminate\Support\Collection;
-use Laravel\Nova\Fields\Boolean; 
+use Laravel\Nova\Fields\{Number, Boolean}; 
 use OptimistDigital\MultiselectField\Multiselect; 
 use Armincms\Sofre\Helper; 
+use Armincms\Nova\Fields\Money;
+use Armincms\Sofre\Nova\Setting;
 
 class Menu 
 { 
@@ -13,6 +15,9 @@ class Menu
     { 
         return Collection::make(Helper::days())
                 ->map([$this, 'meals'])
+                ->prepend($this->priceField())
+                ->prepend(Number::make(__('Order'), 'order'))
+                ->prepend(Number::make(__('Duration'), 'duration'))
                 ->prepend(Boolean::make(__("Available"), 'available'))
                 ->all();
     }
@@ -25,5 +30,12 @@ class Menu
                     ->resolveUsing(function($value) {
                         return explode(',', $value);
                     }); 
+    }
+
+    public function priceField()
+    {
+        return Money::make(__('Price'), Setting::option('_sfore_currency_', 'IRR'), 'price')
+                    ->default(0.00)
+                    ->storedInMinorUnits(); 
     }
 }
