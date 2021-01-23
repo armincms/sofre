4d331475443af074a187b4a7ab5216eb955396e9
Ismail Zare <zarehesmaiel@gmail.com>
2021-01-23 12:08:02 +0330

    Refactorings discount apply

--- src/Models/DiscountCollection.php
+++ src/Models/DiscountCollection.php
@@ -7,41 +7,39 @@ use Illuminate\Database\Eloquent\Collection;
 class DiscountCollection extends Collection
 {   
     /**
-     * Get the price with discount  for the given food.
+     * Get the price with discount  for the given price.
      * 
-     * @param  \Armincms\Sofre\Food $food
+     * @param  integer price
      * @return float
      */
-    public function applyOn(Food $food)
-    {
-        $price = floatval(optional($food->pivot)->price); 
-
-        return with(floatval($price - $this->discountAmount($food)), function($price) {
+    public function applyOn($price)
+    { 
+        return with(floatval($price - $this->discountAmount($price)), function($price) {
             return $price > 0 ? $price : 0;
         });
     } 
 
     /**
-     * Get the discounted amount for the given food.
+     * Get the discounted amount for the given price.
      * 
-     * @param  \Armincms\Sofre\Food $food
+     * @param  integer price
      * @return float
      */
-    public function discountAmount(Food $food)
+    public function discountAmount($price)
     {
-        return $this->sum->discountAmount(floatval(optional($food->pivot)->price));
+        return $this->sum->discountAmount(floatval($price));
     }  
 
     /**
-     * Get the discount percentage for the given food.
+     * Get the discount percentage for the given price.
      * 
-     * @param  \Armincms\Sofre\Food $food
+     * @param  integer price
      * @return float
      */
-    public function discountPercent(Food $food)
+    public function discountPercent($price)
     {
-        $discountAmount = $this->discountAmount($food);
-        $price = floatval(optional($food->pivot)->price); 
+        $discountAmount = $this->discountAmount($price);
+        $price = floatval($price); 
 
         return $discountAmount >= $price ? 100 : ($discountAmount / $price) * 100;
     } 
